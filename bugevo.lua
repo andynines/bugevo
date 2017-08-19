1431be08cc3ae51d4d83a919a83ec31d28061083
andynines <andrewsenin@comcast.net>
2017-08-19 00:33:18 -0400

    Update bugevo.lua

--- bugevo.lua
+++ bugevo.lua
@@ -30,28 +30,29 @@ local CODES = {EMPTY=1, --Number codes indicating a certain object on a field sp
                BUG=2,
                BACTERIA=3}
 
-local INITIAL_BACTERIA = 25 --Bacteria already on the field at iteration 0
+local INITIAL_BACTERIA = 100 --Bacteria already on the field at iteration 0
 local BACTERIA_REPLENISH = 1 --How many bacteria appear per iteration
 
-local INITIAL_BUGS = 25 --Bugs already on the field at iteration 0
+local INITIAL_BUGS = 20 --Bugs already on the field at iteration 0
 local MAX_AGE = 1000 --Maximum iterations a bug can undergo before it dies
 
-local INITIAL_ENERGY = 300 --Amount of energy that every bug is initialized with
-local MAX_ENERGY = 750 --Maximum amount of energy a bug can hold
+local INITIAL_ENERGY = 650 --Amount of energy that every bug is initialized with
+local MAX_ENERGY = 1000 --Maximum amount of energy a bug can hold
 local MOVE_ENERGY_CONSUMPTION = 1 --How much energy it takes a bug to undergo one iteration
-local EAT_ENERGY_GAIN = 30 --How much energy a bug gains from consuming one bacteria
+local EAT_ENERGY_GAIN = 25 --How much energy a bug gains from consuming one bacteria
 
-local REPRODUCTIVE_MIN_AGE = 250 --Minimum iterations a bug must have undergone before reproducing
-local REPRODUCTIVE_MIN_ENERGY = 350 --Minimum energy level a bug must hold to reproduce
+local REPRODUCTIVE_MIN_AGE = 300 --Minimum iterations a bug must have undergone before reproducing
+local REPRODUCTIVE_MIN_ENERGY = 435 --Minimum energy level a bug must hold to reproduce
 local OFFSPRING = 2 --How many offspring are created when a bug reproduces
-local MUTATION_RATE = 2 --Change rate of movement probabilities of a bug's offspring
-local DIRECTION_MAX_PROBABILITY = 20 --Largest probability a bug may have to move in a certain direction
+local MUTATION_RATE = 1 --Change rate of movement probabilities of a bug's offspring
+local DIRECTION_MAX_PROBABILITY = 9 --Largest probability a bug may have to move in a certain direction
 
 local function xy(x, y)
-    --Coordinate pair objects
+    --Create coordinate pair objects
     return {x=x, y=y}
 end
 
+local RIGID_BORDERS = false --If false, bugs may cross edges and appear on the opposite side
 local MOVE_DELTAS = {xy(0, 1), --Coordinate pair addends for every direction of movement
                      xy(0, -1),
                      xy(1, 0),
@@ -61,14 +62,20 @@ local MOVE_DELTAS = {xy(0, 1), --Coordinate pair addends for every direction of 
 		     xy(-1, 1),
 		     xy(-1, -1)}
 
-math.randomseed(os.time())
+local setmetatable = setmetatable
+local insert = table.insert
+local remove = table.remove
+local random = math.random
+local randomseed = math.randomseed
+
+randomseed(os.time())
 
 local field
 local bugs
 
 local function within_borders(position)
     --Verifies whether a position is within the field
-    return position.x > 0 and position.x <= FIELD_X and position.y > 0 and position.y <= FIELD_Y
+    return xy(position.x > 0 and position.x <= FIELD_X, position.y > 0 and position.y <= FIELD_Y)
 end
 
 local function get_position()
@@ -79,7 +86,7 @@ local function get_position()
     local max_attempts = FIELD_X * FIELD_Y
     local check_x, check_y
     repeat
-        check_x, check_y = math.random(1, FIELD_X), math.random(1, FIELD_Y)
+        check_x, check_y = random(1, FIELD_X), random(1, FIELD_Y)
         if field[check_x][check_y] == CODES.EMPTY then
             return xy(check_x, check_y)
         end
@@ -94,8 +101,9 @@ local function get_adjacents(position)
     for delta = 1, #MOVE_DELTAS do
         local current_delta = MOVE_DELTAS[delta]
         local check_position = xy(position.x + current_delta.x, position.y + current_delta.y)
-        if within_borders(check_position) and field[check_position.x][check_position.y] == CODES.EMPTY then
-	    table.insert(adjacents, current_delta)
+	local border_check = within_borders(check_position)
+        if (border_check.x and border_check.y) and field[check_position.x][check_position.y] == CODES.EMPTY then
+	    insert(adjacents, current_delta)
         end
     end
     return adjacents
@@ -116,7 +124,7 @@ end
 local function mutate(genes)
     --Slightly alter the bug's movement genes
     for direction, probability in pairs(genes) do
-        local new_gene = probability + MUTATION_RATE * math.random(-1, 1)
+        local new_gene = probability + MUTATION_RATE * random(-1, 1)
         if new_gene < 0 then
             new_gene = 0
         elseif new_gene > DIRECTION_MAX_PROBABILITY then
@@ -152,7 +160,7 @@ function Bug:new(heredity, spawn_point)
     else
 	self.genes = {}
         for direction = 1, #MOVE_DELTAS do
-            table.insert(self.genes, math.random(0, DIRECTION_MAX_PROBABILITY))
+            insert(self.genes, random(0, DIRECTION_MAX_PROBABILITY))
 	end
     end
     return self
@@ -165,17 +173,32 @@ function Bug:wander()
     for direction, probability in pairs(self.genes) do
         directions = directions + 1
         for chance = 1, probability do
-            table.insert(probabilities, directions)
+            insert(probabilities, directions)
         end
     end
     --Bug finds nowhere to move
     if not (#probabilities > 0) then
         return nil
     end
-    local movement_delta = MOVE_DELTAS[probabilities[math.random(1, #probabilities)]]
+    local movement_delta = MOVE_DELTAS[probabilities[random(1, #probabilities)]]
     local new_position = xy(self.position.x + movement_delta.x, self.position.y + movement_delta.y)
     --Move to new location if possible
-    if not within_borders(new_position) or field[new_position.x][new_position.y] == CODES.BUG then
+    local border_check = within_borders(new_position)
+    if (not border_check.x) or (not border_check.y) then
+	if RIGID_BORDERS then
+	    return nil
+	else
+	    --Screen wrap the bug if allowed
+	    if not border_check.x then
+		new_position.x = new_position.x - movement_delta.x * FIELD_X
+	    end
+	    if not border_check.y then
+		new_position.y = new_position.y - movement_delta.y * FIELD_Y
+	    end
+	end
+    end
+    local position_check = field[new_position.x][new_position.y]
+    if not (position_check == CODES.EMPTY or position_check == CODES.BACTERIA) then
         return nil
     end
     field[self.position.x][self.position.y] = CODES.EMPTY
@@ -205,7 +228,7 @@ local function initialize()
     for bug_count = 1, INITIAL_BUGS do
         local new_bug = Bug:new()
         if new_bug then
-            table.insert(bugs, new_bug)
+            insert(bugs, new_bug)
         else
             break
         end
@@ -224,19 +247,19 @@ local function iterate()
         --Kill old and starving bugs
         local bug = bugs[bug_index]
         if bug.age > MAX_AGE or bug.energy <= 0 then
-            table.insert(dead_indexes, bug_index)
+            insert(dead_indexes, bug_index)
         --Allow healthy, sufficiently aged bugs to reproduce if they have the space
         elseif bug.age >= REPRODUCTIVE_MIN_AGE and bug.energy >= REPRODUCTIVE_MIN_ENERGY then
             local adjacents = get_adjacents(bug.position)
             if #adjacents >= OFFSPRING then
                 local offspring = 0
                 repeat
-                    spawn_delta = table.remove(adjacents, math.random(1, #adjacents))
-                    table.insert(bugs, Bug:new(bug.genes, xy(bug.position.x + spawn_delta.x, bug.position.y + spawn_delta.y)))
+                    spawn_delta = remove(adjacents, random(1, #adjacents))
+                    insert(bugs, Bug:new(bug.genes, xy(bug.position.x + spawn_delta.x, bug.position.y + spawn_delta.y)))
                     offspring = offspring + 1
                 until offspring == OFFSPRING
                 --Bugs die after reproducing
-                table.insert(dead_indexes, bug_index)
+                insert(dead_indexes, bug_index)
             end
         end
     end
@@ -245,7 +268,7 @@ local function iterate()
         local dead_bug = dead_indexes[1]
         local corpse = bugs[dead_bug]
         field[corpse.position.x][corpse.position.y] = CODES.EMPTY
-        table.remove(bugs, dead_bug)
+        remove(bugs, dead_bug)
     end
     --End simulation if population dies
     if #bugs == 0 then
