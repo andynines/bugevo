830d3040d93ea080281652ec891a2467bdd4b1cf
andynines <andrewsenin@comcast.net>
2017-08-29 16:40:48 -0400

    Update bugevo.lua

--- bugevo.lua
+++ bugevo.lua
@@ -24,43 +24,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]--
 
-local FIELD_X = 180 --Width of environment
-local FIELD_Y = 180 --Height of environment
+local FIELD_X = 100
+local FIELD_Y = 100
 local CODES = {EMPTY=1, --Number codes indicating a certain object on a field space
                BUG=2,
                BACTERIA=3}
 
-local INITIAL_BACTERIA = 100 --Bacteria already on the field at iteration 0
-local BACTERIA_REPLENISH = 1 --How many bacteria appear per iteration
+local INITIAL_BACTERIA = 200 --Bacteria already on the field at iteration 0
+local BACTERIA_REPLENISH_WAIT = 3 --How many iterations between a new bacteria spawn
+local BACTERIA_REPLENISH_QUANTITY = 1 --How many bacteria spawn upon replenishment
 
 local INITIAL_BUGS = 20 --Bugs already on the field at iteration 0
 local MAX_AGE = 1000 --Maximum iterations a bug can undergo before it dies
 
-local INITIAL_ENERGY = 650 --Amount of energy that every bug is initialized with
-local MAX_ENERGY = 1000 --Maximum amount of energy a bug can hold
+local INITIAL_ENERGY = 200 --Amount of energy that every bug is initialized with
+local MAX_ENERGY = 1500 --Maximum amount of energy a bug can hold
 local MOVE_ENERGY_CONSUMPTION = 1 --How much energy it takes a bug to undergo one iteration
-local EAT_ENERGY_GAIN = 25 --How much energy a bug gains from consuming one bacteria
+local EAT_ENERGY_GAIN = 100 --How much energy a bug gains from consuming one bacteria
 
-local REPRODUCTIVE_MIN_AGE = 300 --Minimum iterations a bug must have undergone before reproducing
-local REPRODUCTIVE_MIN_ENERGY = 435 --Minimum energy level a bug must hold to reproduce
+local REPRODUCTIVE_MIN_AGE = 800 --Minimum iterations a bug must have undergone before reproducing
+local REPRODUCTIVE_MIN_ENERGY = 1000 --Minimum energy level a bug must hold to reproduce
 local OFFSPRING = 2 --How many offspring are created when a bug reproduces
 local MUTATION_RATE = 1 --Change rate of movement probabilities of a bug's offspring
-local DIRECTION_MAX_PROBABILITY = 9 --Largest probability a bug may have to move in a certain direction
+local DIRECTION_MAX_PROBABILITY = 100 --Largest probability a bug may have to move in a certain direction
 
 local function xy(x, y)
     --Create coordinate pair objects
     return {x=x, y=y}
 end
 
-local RIGID_BORDERS = false --If false, bugs may cross edges and appear on the opposite side
-local MOVE_DELTAS = {xy(0, 1), --Coordinate pair addends for every direction of movement
-                     xy(0, -1),
-                     xy(1, 0),
-                     xy(-1, 0),
-		     xy(1, 1),
-		     xy(1, -1),
-		     xy(-1, 1),
-		     xy(-1, -1)}
+local RIGID_BORDERS = false --If false, bugs can cross edges and will appear on the opposite side
+local MOVEMENT_DISTANCE = 1 --How many spaces a bug can move in one iteration
+local MOVE_DELTAS = {xy(0, MOVEMENT_DISTANCE), --Coordinate pair addends for every direction of movement
+                     xy(0, -MOVEMENT_DISTANCE),
+                     xy(MOVEMENT_DISTANCE, 0),
+                     xy(-MOVEMENT_DISTANCE, 0),
+                     xy(MOVEMENT_DISTANCE, MOVEMENT_DISTANCE),
+                     xy(MOVEMENT_DISTANCE, -MOVEMENT_DISTANCE),
+                     xy(-MOVEMENT_DISTANCE, MOVEMENT_DISTANCE),
+                     xy(-MOVEMENT_DISTANCE, -MOVEMENT_DISTANCE)}
+
+local SAMPLE_INTERVAL = 200 --Print information about a random bug at this interval
 
 local setmetatable = setmetatable
 local insert = table.insert
@@ -72,6 +76,8 @@ randomseed(os.time())
 
 local field
 local bugs
+local replenish_timer
+local iteration
 
 local function within_borders(position)
     --Verifies whether a position is within the field
@@ -82,28 +88,27 @@ local function get_position()
     --Find and return an empty space on the field
     local field = field
     local attempts = 0
-    --Built with edge case in mind that there are no empty spaces
     local max_attempts = FIELD_X * FIELD_Y
-    local check_x, check_y
     repeat
-        check_x, check_y = random(1, FIELD_X), random(1, FIELD_Y)
+        local check_x, check_y = random(1, FIELD_X), random(1, FIELD_Y)
         if field[check_x][check_y] == CODES.EMPTY then
             return xy(check_x, check_y)
         end
         attempts = attempts + 1
+    --Edge case: there are no empty spaces left
     until attempts == max_attempts
     return nil
 end
 
 local function get_adjacents(position)
-    --Return a table of available directions to move adjacent to a given position
+    --Return a table of deltas to move to available adjacent locations of a position
     local adjacents = {}
     for delta = 1, #MOVE_DELTAS do
         local current_delta = MOVE_DELTAS[delta]
         local check_position = xy(position.x + current_delta.x, position.y + current_delta.y)
-	local border_check = within_borders(check_position)
+        local border_check = within_borders(check_position)
         if (border_check.x and border_check.y) and field[check_position.x][check_position.y] == CODES.EMPTY then
-	    insert(adjacents, current_delta)
+            insert(adjacents, current_delta)
         end
     end
     return adjacents
@@ -122,7 +127,7 @@ local function spawn_bacteria(quantity)
 end
 
 local function mutate(genes)
-    --Slightly alter the bug's movement genes
+    --Slightly alter a bug's movement genes
     for direction, probability in pairs(genes) do
         local new_gene = probability + MUTATION_RATE * random(-1, 1)
         if new_gene < 0 then
@@ -139,9 +144,14 @@ end
 local Bug = {}
 Bug.__index = Bug
 
-function Bug:new(heredity, spawn_point)
+function Bug:new(parent_generation, spawn_point, heredity)
     local self = {}
     setmetatable(self, Bug)
+    if parent_generation then
+        self.generation = parent_generation + 1
+    else
+        self.generation = 1
+    end
     self.age = 0
     self.energy = INITIAL_ENERGY
     if spawn_point then
@@ -158,12 +168,26 @@ function Bug:new(heredity, spawn_point)
     if heredity then
         self.genes = mutate(heredity)
     else
-	self.genes = {}
+        self.genes = {}
         for direction = 1, #MOVE_DELTAS do
             insert(self.genes, random(0, DIRECTION_MAX_PROBABILITY))
-	end
+        end
     end
     return self
+end
+
+function Bug:info()
+    --Print the bug's attributes
+    print("bug info"..
+          "\ngeneration ".. self.generation..
+          "\nage ".. self.age..
+          "\nenergy ".. self.energy..
+          "\nlocation (".. self.position.x.. ", ".. self.position.y.. ")")
+    for probability_index, probability in pairs(self.genes) do
+        local current_delta = MOVE_DELTAS[probability_index]
+        print("gene ".. "(".. current_delta.x.. ", ".. current_delta.y.. ") ".. probability.. "/".. DIRECTION_MAX_PROBABILITY)
+    end
+    print("end info\n")
 end
 
 function Bug:wander()
@@ -185,17 +209,17 @@ function Bug:wander()
     --Move to new location if possible
     local border_check = within_borders(new_position)
     if (not border_check.x) or (not border_check.y) then
-	if RIGID_BORDERS then
-	    return nil
-	else
-	    --Screen wrap the bug if allowed
-	    if not border_check.x then
-		new_position.x = new_position.x - movement_delta.x * FIELD_X
-	    end
-	    if not border_check.y then
-		new_position.y = new_position.y - movement_delta.y * FIELD_Y
-	    end
-	end
+        if RIGID_BORDERS then
+            return nil
+        else
+            --Screen wrap the bug if allowed
+            local field_dimensions = xy(FIELD_X, FIELD_Y)
+            for axis_index, axis in pairs({"x", "y"}) do
+                if not border_check[axis] then
+                    new_position[axis] = new_position[axis] - movement_delta[axis] * field_dimensions[axis]
+                end
+            end
+        end
     end
     local position_check = field[new_position.x][new_position.y]
     if not (position_check == CODES.EMPTY or position_check == CODES.BACTERIA) then
@@ -212,6 +236,24 @@ function Bug:wander()
     end
     field[self.position.x][self.position.y] = CODES.BUG
     return true
+end
+
+function Bug:reproduce()
+    --Spawn slightly mutated children adjacent to a bug
+    local adjacents = get_adjacents(self.position)
+    if #adjacents >= OFFSPRING then
+        local offspring = 0
+            repeat
+                spawn_delta = remove(adjacents, random(1, #adjacents))
+                insert(bugs, Bug:new(self.generation,
+                                     xy(self.position.x + spawn_delta.x, self.position.y + spawn_delta.y,
+                                     self.genes)))
+                offspring = offspring + 1
+            until offspring == OFFSPRING
+        return true
+    else
+        return nil
+    end
 end
 
 local function initialize()
@@ -235,6 +277,8 @@ local function initialize()
     end
     --Create initial supply of bacteria
     spawn_bacteria(INITIAL_BACTERIA)
+    replenish_timer = 0
+    iteration = 0
 end
 
 local function iterate()
@@ -242,6 +286,7 @@ local function iterate()
     local dead_indexes = {}
     for bug_index = 1, #bugs do
         bugs[bug_index]:wander()
+        --Bug matures
         bugs[bug_index].age = bugs[bug_index].age + 1
         bugs[bug_index].energy = bugs[bug_index].energy - MOVE_ENERGY_CONSUMPTION
         --Kill old and starving bugs
@@ -250,14 +295,7 @@ local function iterate()
             insert(dead_indexes, bug_index)
         --Allow healthy, sufficiently aged bugs to reproduce if they have the space
         elseif bug.age >= REPRODUCTIVE_MIN_AGE and bug.energy >= REPRODUCTIVE_MIN_ENERGY then
-            local adjacents = get_adjacents(bug.position)
-            if #adjacents >= OFFSPRING then
-                local offspring = 0
-                repeat
-                    spawn_delta = remove(adjacents, random(1, #adjacents))
-                    insert(bugs, Bug:new(bug.genes, xy(bug.position.x + spawn_delta.x, bug.position.y + spawn_delta.y)))
-                    offspring = offspring + 1
-                until offspring == OFFSPRING
+            if bug:reproduce() then
                 --Bugs die after reproducing
                 insert(dead_indexes, bug_index)
             end
@@ -275,8 +313,17 @@ local function iterate()
         return nil
     end
     --Replenish bacteria
-    spawn_bacteria(BACTERIA_REPLENISH)
-    return field
+    replenish_timer = replenish_timer + 1
+    if replenish_timer == BACTERIA_REPLENISH_WAIT then
+        spawn_bacteria(BACTERIA_REPLENISH_QUANTITY)
+        replenish_timer = 0
+    end
+    iteration = iteration + 1
+    --Sample the population
+    if iteration % SAMPLE_INTERVAL == 0 then
+        bugs[math.random(1, #bugs)]:info()
+    end
+    return {field=field, iteration=iteration, population=#bugs}
 end
 
 --Information necessary for visual representation
