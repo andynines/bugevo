ffe6e08d819e13f64c4d3283359d58938b02cbea
andynines <andrewsenin@comcast.net>
2017-09-08 20:41:08 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -31,14 +31,12 @@ local iterate = simulation.iterate
 
 local MAX_ITERATIONS = 30000 --End the program after this many iterations
 
-local exit = os.exit
-
 initialize()
 print("Simulation will cease after ".. MAX_ITERATIONS.. " iterations\n")
 for to_max = 1, MAX_ITERATIONS do
     if not iterate() then
         print("The bugs went extinct")
-        exit()
+        os.exit()
     end
 end
 print("Maximum iterations reached")
