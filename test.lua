5cfb32c1ac16c78847c0bb8a6d8a88e32addea4d
andynines <andrewsenin@comcast.net>
2017-10-23 22:49:28 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,21 +2,31 @@
 test.lua
 Copyright (c) 2017 andynines
 MIT License
-]]--
+]]
 
 local simulation = require("bugevo")
+
+local MAX_ITERATIONS = 30000 --Simulation will end at or before this many iterations
+local SAMPLE_INTERVAL = 500 --A bug's attributes are printed at this iteration period
 
 local initialize = simulation.initialize
 local iterate = simulation.iterate
 
-local MAX_ITERATIONS = 30000 --End the program after this many iterations
-
-initialize()
-print("Simulation will cease after ".. MAX_ITERATIONS.. " iterations\n")
-for to_max = 1, MAX_ITERATIONS do
-    if not iterate() then
-        print("The bugs went extinct")
-        os.exit()
-    end
+local function test()
+	initialize()
+	print("Simulation will cease after ".. MAX_ITERATIONS.. " iterations\n")
+	for _ = 1, MAX_ITERATIONS do
+		local data = iterate()
+	    if not data then
+			--Simulation returning nil means population has died
+	        print("The bugs went extinct")
+	        break
+	    elseif data.iteration % SAMPLE_INTERVAL == 0 then
+			--Routinely provide data on a sample bug
+			io.write(data.bugs[math.random(1, #data.bugs)]:info().. "\n")
+	    end
+	end
+	print("Maximum iterations reached")
 end
-print("Maximum iterations reached")
+
+test()
