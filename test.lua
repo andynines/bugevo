600ae0338820accbf074b82952e1fe9b532aebdc
andynines <andrewsenin@comcast.net>
2017-12-20 22:51:18 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,27 +6,26 @@ MIT License
 
 local simulation = require("bugevo")
 
-local MAX_ITERATIONS = 30000 --Simulation will end at or before this many iterations
+local MAX_ITERATIONS = 10000 --Simulation will end at or before this many iterations
 local SAMPLE_INTERVAL = 500 --A bug's attributes are printed at this iteration period
 
 local initialize = simulation.initialize
 local iterate = simulation.iterate
 
 local function test()
-    initialize()
-    print("Simulation will cease after ".. MAX_ITERATIONS.. " iterations\n")
-    for _ = 1, MAX_ITERATIONS do
-        local data = iterate()
-        if not data then
-            --Simulation returning nil means population has died
-            print("The bugs went extinct")
-            break
-        elseif data.iteration % SAMPLE_INTERVAL == 0 then
-            --Routinely provide data on a sample bug
-            io.write(data.bugs[math.random(1, #data.bugs)]:info().. "\n")
-        end
-    end
-    print("Maximum iterations reached")
+	initialize()
+	io.write("Simulation will terminate after ".. MAX_ITERATIONS.. " iterations\n\n")
+	for _ = 1, MAX_ITERATIONS do
+		local data = iterate()
+	    if not data then
+			--Simulation returning nil means population has died
+	        return "The bugs went extinct"
+	    elseif data.iteration % SAMPLE_INTERVAL == 0 then
+			--Routinely provide data on a random bug
+			io.write(data.bugs[math.random(1, #data.bugs)]:info().. "\n")
+	    end
+	end
+	return "Maximum iterations reached"
 end
 
-test()
+io.write(test().. "\nSimulation terminated\n")
