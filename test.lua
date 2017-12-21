aac82ebf9689b59b14052f5f907c9ad09b9742ca
andynines <andrewsenin@comcast.net>
2017-12-20 22:57:58 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -13,19 +13,19 @@ local initialize = simulation.initialize
 local iterate = simulation.iterate
 
 local function test()
-	initialize()
-	io.write("Simulation will terminate after ".. MAX_ITERATIONS.. " iterations\n\n")
-	for _ = 1, MAX_ITERATIONS do
-		local data = iterate()
-	    if not data then
-			--Simulation returning nil means population has died
-	        return "The bugs went extinct"
-	    elseif data.iteration % SAMPLE_INTERVAL == 0 then
-			--Routinely provide data on a random bug
-			io.write(data.bugs[math.random(1, #data.bugs)]:info().. "\n")
-	    end
-	end
-	return "Maximum iterations reached"
+    initialize()
+    io.write("Simulation will terminate after ".. MAX_ITERATIONS.. " iterations\n\n")
+    for _ = 1, MAX_ITERATIONS do
+        local data = iterate()
+        if not data then
+            --Simulation returning nil means population has died
+            return "The bugs went extinct"
+        elseif data.iteration % SAMPLE_INTERVAL == 0 then
+            --Routinely provide data on a random bug
+            io.write(data.bugs[math.random(1, #data.bugs)]:info().. "\n")
+        end
+    end
+    return "Maximum iterations reached"
 end
 
 io.write(test().. "\nSimulation terminated\n")
